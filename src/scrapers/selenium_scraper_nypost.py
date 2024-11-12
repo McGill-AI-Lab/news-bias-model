004c8be328dcd8077060afe9929d24d956b7d4ea
Jacob <Jacob.leader06@gmail.com>
2024-11-12 16:39:11 -0500

    scrapers

---
+++ src/scrapers/selenium_scraper_nypost.py
@@ -0,0 +1,57 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+import json
+import time
+
+#! THIS CODE ONLY GETS URLS
+
+# Set up the Selenium WebDriver (you need to have ChromeDriver installed for Chrome)
+driver = webdriver.Chrome()  # or webdriver.Firefox() for Firefox
+query_list = ['palestine', 'israel', 'gaza', 'West Bank', 'hamas', 'Jihad']
+
+url_set = []
+i = 1
+
+# Load the CNN search page
+for query in query_list:
+    print(f'Starting to query "{query}"')
+    while True:
+        i += 1
+        url = f'https://nypost.com/search/{query}/page/{i}/'
+        
+        try:
+            driver.get(url)
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "search-results"))
+            )
+        except:
+            print("Timeout or no content available. Exiting loop.")
+            break
+        
+        # Parse with BeautifulSoup
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        story_links = [a['href'] for a in soup.select('.search-results__story .story__headline a')]
+        
+        if story_links:
+            url_set.extend(story_links)
+            print(f"Got {len(story_links)}!")
+        else:
+            break
+
+driver.quit()
+print(f"got {len(url_set)} links!")
+
+links_list = list(set(url_set))
+with open('urls.txt', 'a') as f:
+    for url in links_list:
+        f.write(f"{url}\n")
+
+data = {
+    "nypost.com": links_list
+}
+with open('data/news-data.json', 'w') as f:
+    json.dumps(data, f, indent=4)
+        
