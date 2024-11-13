8fbe3ac8d35b0103acda1ef06decebbb066ee0b3
Jacob <Jacob.leader06@gmail.com>
2024-11-12 22:41:30 -0500

    minor fixes

--- src/scrapers/selenium_scraper_nypost.py
+++ src/scrapers/selenium_scraper_nypost.py
@@ -9,23 +9,23 @@ import time
 #! THIS CODE ONLY GETS URLS
 
 # Set up the Selenium WebDriver (you need to have ChromeDriver installed for Chrome)
-driver = webdriver.Chrome()  # or webdriver.Firefox() for Firefox
-query_list = ['palestine', 'israel', 'gaza', 'West Bank', 'hamas', 'Jihad']
+driver = webdriver.Chrome()
+query_list = ['palestine', 'israel', 'gaza', 'hamas', 'Jihad']
 
 url_set = []
-i = 1
 
 # Load the CNN search page
 for query in query_list:
     print(f'Starting to query "{query}"')
+    i = 0
     while True:
         i += 1
         url = f'https://nypost.com/search/{query}/page/{i}/'
         
         try:
             driver.get(url)
-            WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "search-results"))
+            WebDriverWait(driver, 30).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "page__content"))
             )
         except:
             print("Timeout or no content available. Exiting loop.")
