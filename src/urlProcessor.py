89168b21b1252c9447ba2bd87210403b21445988
Jacob <Jacob.leader06@gmail.com>
2024-11-22 22:35:02 -0500

    batch processing

--- src/urlProcessor.py
+++ src/urlProcessor.py
@@ -2,22 +2,27 @@ import newspaper
 import json
 from tqdm import tqdm
 from multiprocessing import Pool
-import unidecode
-
+import time
+# import unidecode
 
 def getData(path):
     with open(path, 'r') as f:
         url_dict = json.load(f)
-    
-    return url_dict
-    
-def processArticle(url):
-    paper = newspaper.article(url) # Makes a new Article instance
 
-    title = paper.title 
+    return url_dict
+
+def processArticle(url):
+    try:
+        paper = newspaper.article(url) # Makes a new Article instance
+    except Exception as e:
+        # print(f"Error processing {url}: {e}")
+        return None  # Return None if an error occurs
+
+    title = paper.title
     authors = paper.authors
     date = paper.publish_date
     text = paper.text
+
     newspaper_data = {
         "url": url,
         "title": cleanText(title),
@@ -25,8 +30,9 @@ def processArticle(url):
         "date": str(date),
         "text": cleanText(text)
     }
+
     return newspaper_data
-    
+
 
 def cleanText(text):
     clean_text = text.encode("ascii", "ignore").decode() # Gets rid of chars that arent ASCII ex. accented chars
@@ -36,46 +42,63 @@ def cleanText(text):
 
 def extractData(url_list):
     data = []
-    pool = Pool(processes=32)  # may be too high, not sure
+    # pool = Pool(processes=10)
+    batches = batchUrls(url_list, 1000)
 
-    # tqdm gives us a progress bar, args are self-explanitory
-    with tqdm(total=len(url_list), desc=f"Extracting Article Data") as pbar:
-        for url in url_list:
-            pool.apply_async(
-                processArticle,  # process article asynchronously
-                args=(url,), # args expects a tuple, so we give it a tuple with one item
-                callback=lambda single_newspaper_data: appendData(data, single_newspaper_data, pbar) # appends the result after processArticle has run, passes data by reference, so we can update it
-            )
+    for batch_num, batch in enumerate(batches):
+        # time.sleep(10)
+        with tqdm(total=len(batch), desc=f"Extracting Article Data (batch {batch_num + 1})") as pbar:
+            with Pool(processes=32) as pool:
+                for url in batch:
+                    pool.apply_async(
+                        processArticle,  # process article asynchronously
+                        args=(url,),  # args expects a tuple, so we give it a tuple with one item
+                        callback=lambda single_newspaper_data: appendData(data, single_newspaper_data, pbar) # appends the result after processArticle has run, passes data by reference, so we can update it                    )
+                    )
 
-        pool.close()
-        pool.join()  # Wait for all processes to finish
+                # print("DONE, CLOSING")
+                pool.close()
+                pool.join()
+
+    # Wait for all processes to finish
 
     return data
+
+def batchUrls(urls, batch_size):
+    batches = []
+    start_num = 0
+
+    while start_num < len(urls):
+        batch = urls[start_num:start_num + batch_size]
+        batches.append(batch)
+        start_num += batch_size  # Move to the next batch
+
+    print(f"Made {len(batches)} batches of {batch_size} urls each")
+
+    return batches
 
 def appendData(data, single_newspaper_data, pbar):
     data.append(single_newspaper_data)
     pbar.update(1)
-    
+
 def main(url_dict):
     data = {}
     keys = url_dict.keys()
-    
+
     for key in keys:
         print(f"Now extracting the {key} urls...")
         url_list = url_dict.get(key, [])
         extracted_data = extractData(url_list) # starts async pool with all urls passed
         data[key] = extracted_data # saved extracted data as value for the key which is the same as the key the urls were from
-        
+
     return data
 
 def writeData(data, path="src/data/news-data-extracted.json"):
     with open(path, 'w') as f: # 'w' means 'open for writing, trucates the file first'
         json.dump(data, f, indent=4)
-        
+
 if __name__ == "__main__":
     url_json = getData("src/data/article_urls.json")
     data = main(url_json)
-    # print(data)
     writeData(data, "src/data/news-data-extracted.json")
-    
-
+    print("Completed!")
