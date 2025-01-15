7f23e2e933c9b738d201b3de8b93984c6be29f13
Jacob <Jacob.leader06@gmail.com>
2025-01-15 12:50:26 -0500

    small refac + remove old code

--- src/bias-analysis/bias-analysis.py
+++ src/bias-analysis/bias-analysis.py
@@ -20,7 +20,7 @@ class FileHandler():
         """
         Takes in the file of extracted news and the newspaper name.
         
-        Outputs: [str] || list of articles from the newspaper source
+        returns: [str] || list of articles from the newspaper source
         """
 
         with open(self.newspaper_data_path, "r") as json_file:
@@ -60,6 +60,8 @@ class FileHandler():
     def load_preprocessed_newspapers(self):
         """
         Load preprocessed newspapers from a JSON file.
+        
+        returns: {} containing preprocessed newspapers
         """
         if os.path.exists(self.preprocessed_newspapers_path):
             try:
@@ -81,18 +83,12 @@ class FileHandler():
 
 # Create helper functions to analyze our newspaper and corpus. These functions will help us see: 
 # - how many articles are there in the newspaper about our selected topic (Israel-Palestine) since we only scraped relevant articles
-# - corpus size (word count) before and after preprocessing, which will result in some reductions
 # - number of unique words and sentences
-# - number of occurance of a unique word in the preprocessed corpus
-
-# create get_article_number, corpus size, and other helper functions
 
 class BiasAnalysis():
     def __init__(self):
         self.file_handler = FileHandler()
         self.preprocessor = Preprocessor()
-
-        self.preprocessed_articles = ...
 
     def no_of_articles(self, article_list):
         return len(article_list)
@@ -228,7 +224,7 @@ class BiasAnalysis():
 
 
 
-    def main(self, newspaper_list, file_handler, preprocessor):
+    def main(self, newspaper_list):
         """
         1. Get a list of newspapers
         2. Create a dictionary of newspapers, each being a dictionary -> {__: {},__:{},__:{}}
@@ -242,7 +238,7 @@ class BiasAnalysis():
             bias score for palestine, israel, hamas, idf, etc,
         """
 
-        preprocessed_newspapers = file_handler.load_preprocessed_newspapers()
+        preprocessed_newspapers = self.file_handler.load_preprocessed_newspapers()
 
 
         # check if the newspaper is already preprocessed, if it is skip it
@@ -251,8 +247,8 @@ class BiasAnalysis():
                 preprocessed_newspapers[newspaper] = {}
                 dict_newspaper = preprocessed_newspapers[newspaper]
 
-                article_list = file_handler.create_article_list(newspaper)
-                sentence_list = preprocessor.preprocess_newspaper(article_list)
+                article_list = self.file_handler.create_article_list(newspaper)
+                sentence_list = self.preprocessor.preprocess_newspaper(article_list)
 
                 dict_newspaper["no_of_articles"] = self.no_of_articles(article_list)
                 dict_newspaper["corpus_size_before_preprocess"] = self.corpus_size_before(article_list)
@@ -304,7 +300,7 @@ class BiasAnalysis():
                 dict_newspaper["palestine-israel_score"] = dict_newspaper["portrayal_palestine_score"] - dict_newspaper["portrayal_israel_score"]
                 print("palestinian are better portrayed by: ", dict_newspaper["palestine-israel_score"])
 
-                file_handler.save_newspaper_dict(preprocessed_newspapers)
+                self.file_handler.save_newspaper_dict(preprocessed_newspapers)
                 
         return preprocessed_newspapers
 
@@ -336,147 +332,12 @@ if __name__ == "__main__":
     newspaper_list = ["cnn.com", "WashingtonPost.com"]
     run_master = run_flow()
     bias_analysis = BiasAnalysis()
-    file_handler = FileHandler()
-    preprocessor = Preprocessor()
+
     if run_master:
-        processed_newspapers = bias_analysis.main(newspaper_list, file_handler, preprocessor)
+        processed_newspapers = bias_analysis.main(newspaper_list)
 
 
     model = Word2Vec.load(f"WashingtonPost.com/WashingtonPost.com_w2v.model")
 
-
     result = model.wv.similarity("attacker", "israeli")
     print(f"Similarity Between 'attacker' and 'israeli' is {result} for washington post")
-
-
-
-
-
-
-
-
-
-
-
-
-# from newspaper import Article # Newspaper3K, NOT 4k
-# import json
-# from tqdm import tqdm
-# from multiprocessing import Pool
-# import time
-# # import unidecode
-
-# def getData(path):
-#     with open(path, 'r') as f:
-#         url_dict = json.load(f)
-
-#     return url_dict
-
-# def processArticle(url):
-#     try:
-#         paper = Article(url) # Makes a new Article instance
-#         paper.download() # Gets the HTML content
-#         paper.parse() # Makes a new Article instance
-#     except Exception as e:
-#         # print(f"Error processing {url}: {e}")
-#         return None  # Return None if an error occurs
-
-#     title = paper.title
-#     authors = paper.authors
-#     date = paper.publish_date
-#     text = paper.text
-
-#     newspaper_data = {
-#         "url": url,
-#         "title": cleanText(title),
-#         "authors": authors,
-#         "date": str(date),
-#         "text": cleanText(text)
-#     }
-
-#     return newspaper_data
-
-
-# def cleanText(text):
-#     clean_text = text.encode("ascii", "ignore").decode() # Gets rid of chars that arent ASCII ex. accented chars
-#     clean_text = clean_text.replace("\n", "").replace("\u2019", "'") # Removes annoying escape sequences
-#     # clean_text = unidecode(clean_text) # Makes it plain text
-#     return clean_text
-
-# def extractData(url_list):
-#     newspaper_source_data = []
-#     # pool = Pool(processes=10)
-#     batches = batchUrls(url_list, 1000)
-#     error_list = []
-
-#     for batch_num, batch in enumerate(batches):
-#         time.sleep(5)
-#         with tqdm(total=len(batch), desc=f"Extracting Article Data (batch {batch_num + 1})") as pbar:
-#             with Pool(processes=4) as pool:
-#                 for url in batch:
-#                     pool.apply_async(
-#                         processArticle,  # process article asynchronously
-#                         args=(url,),  # args expects a tuple, so we give it a tuple with one item
-#                         callback=lambda single_newspaper_data: appendData(newspaper_source_data, single_newspaper_data, pbar, error_list, url) # appends the result after processArticle has run, passes data by reference, so we can update it                    )
-#                     )
-
-#                 # print("DONE, CLOSING")
-#                 pool.close()
-#                 pool.join()
-
-#     # Wait for all processes to finish
-#     return newspaper_source_data, error_list
-
-# def batchUrls(urls, batch_size):
-#     batches = []
-#     start_num = 0
-
-#     while start_num < len(urls):
-#         batch = urls[start_num:start_num + batch_size]
-#         batches.append(batch)
-#         start_num += batch_size  # Move to the next batch
-
-#     print(f"Made {len(batches)} batches of {batch_size} urls each")
-
-#     return batches
-
-# def appendData(data, single_newspaper_data, pbar, error_list, url):
-#     pbar.update(1)
-#     if single_newspaper_data is not None:
-#         data.append(single_newspaper_data)
-#     else:
-#         error_list.append(url)
-
-# def main(url_dict):
-#     data = {}
-#     keys = url_dict.keys()
-#     error_data = {}
-#     for key in keys:
-#         print(f"Now extracting the {key} urls...")
-#         url_list = url_dict.get(key, [])
-#         extracted_data, error_list = extractData(url_list) # starts async pool with all urls passed
-#         data[key] = extracted_data # saved extracted data as value for the key which is the same as the key the urls were from
-#         error_data[key] = error_list
-#         # actualArticles = [article for article in extracted_data if article is not None]
-#         print(f"Out of {len(url_list)} urls, {len(extracted_data)} articles were extracted. ({round((len(extracted_data)/len(url_list)) * 100, 3)}%)")
-
-#     return data, error_data
-
-# def writeData(data, path="fullscrape_extracted.json"):
-#     with open(path, 'w') as f: # 'w' means 'open for writing, trucates the file first'
-#         json.dump(data, f, indent=4)
-
-# if __name__ == "__main__":
-#     url_json = getData("trial_fullscrape.json")
-#     preprocessor = Preprocessor()
-
-#     data, error_data = main(url_json)
-#     print(f"{len(data)} articles extracted in total.")
-#     url_json = getData(r"trial_fullscrape.json")
-#     data = main(url_json)
-#     # print(data)
-#     writeData(data, "fullscrape_extracted.json")
-#     writeData(error_data, "src/data/error_urls.json")
-#     print("Completed!")
-        
-
