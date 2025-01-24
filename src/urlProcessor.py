d2b51509ff85fa2c58ab96d974ba780b245fd772
Jacob <Jacob.leader06@gmail.com>
2025-01-24 18:10:35 -0500

    multithreading added

--- src/urlProcessor.py
+++ src/urlProcessor.py
@@ -1,8 +1,9 @@
 from newspaper import Article # Newspaper3K, NOT 4k
 import json
 from tqdm import tqdm
-from multiprocessing import Pool
+import concurrent.futures
 import time
+import os
 # import unidecode
 
 def getData(path):
@@ -47,21 +48,30 @@ def extractData(url_list):
     # pool = Pool(processes=10)
     batches = batchUrls(url_list, 1000)
     error_list = []
+    num_cores = os.cpu_count()
+
+    batch_times = []
 
     for batch_num, batch in enumerate(batches):
-        time.sleep(5)
         with tqdm(total=len(batch), desc=f"Extracting Article Data (batch {batch_num + 1})") as pbar:
-            with Pool(processes=4) as pool:
-                for url in batch:
-                    pool.apply_async(
-                        processArticle,  # process article asynchronously
-                        args=(url,),  # args expects a tuple, so we give it a tuple with one item
-                        callback=lambda single_newspaper_data: appendData(newspaper_source_data, single_newspaper_data, pbar, error_list, url) # appends the result after processArticle has run, passes data by reference, so we can update it                    )
-                    )
-
-                # print("DONE, CLOSING")
-                pool.close()
-                pool.join()
+            with concurrent.futures.ThreadPoolExecutor(num_cores * 5) as pool:
+                futures = {pool.submit(processArticle, url): url for url in batch}
+                
+                for future in concurrent.futures.as_completed(futures):
+                    url = futures[future] # cause futures is a dictionary of {future:url} (see above)
+                    extracted_article = future.result()
+                    pbar.update(1)
+                    
+                    appendData(newspaper_source_data, extracted_article, error_list, url)
+                    
+        batch_times.append(pbar.format_dict['elapsed'])
+        avg_times = sum(batch_times) / len(batch_times)
+        time_remaining_seconds =  avg_times * (len(batches) - batch_num + 1)
+        print(f"Time remaining ~{(time_remaining_seconds/60):.2f} minutes.")
+        print(f"{len(url_list) - ((batch_num + 1) * len(batch))} of {len(url_list)} articles remaining.")
+        print(f"{((batch_num + 1) * len(batch)) / sum(batch_times):.2f} articles per second on average")
+        
+        
 
     # Wait for all processes to finish
     return newspaper_source_data, error_list
@@ -79,12 +89,11 @@ def batchUrls(urls, batch_size):
 
     return batches
 
-def appendData(data, single_newspaper_data, pbar, error_list, url):
-    pbar.update(1)
+def appendData(data, single_newspaper_data, error_list, url):
     if single_newspaper_data is not None:
         data.append(single_newspaper_data)
     else:
-        error_list.append(url)
+        error_list.append(f"{url} returned an empty article :(")
 
 def main(url_dict):
     data = {}
@@ -106,9 +115,9 @@ def writeData(data, path="src/data/news-data-extracted.json"):
         json.dump(data, f, indent=4)
 
 if __name__ == "__main__":
-    url_json = getData("src/trial_fullscrape.json")
+    url_json = getData("src/washintongpost_links.json")
     data, error_data = main(url_json)
     print(f"{len(data)} articles extracted in total.")
-    writeData(data, "src/data/fullscrape_extracted.json")
+    writeData(data, "washingtonpost_extracted.json")
     writeData(error_data, "src/data/error_urls.json")
     print("Completed!")
